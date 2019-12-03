20edf0c30968e8e5f3ea496d3b099c004c53ed41
Sergey <guber00666@yandex.ru>
2019-12-03 12:45:46 +0300

    tests are implemented

--- test/components/list.test.js
+++ test/components/list.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Books from '../../src/services/books-service'
+import { books } from '../../src/services/books-service'
 import List from "../../src/components/list";
 
 test("Render List without crashing", () => {
     const rootDiv = document.createElement("div");
-    ReactDOM.render(<List data={mockExpenses}/>, rootDiv);
+    ReactDOM.render(<List data={books}/>, rootDiv);
     ReactDOM.unmountComponentAtNode(rootDiv);
 });
